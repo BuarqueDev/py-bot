71c2d4734b3a69552b26fae950fa5f40fd623f6f
Erik Buarque <erikbuarque.10@gmail.com>
2024-07-19 15:12:25 -0300

    primeiro commit

---
+++ teste.py
@@ -0,0 +1,23 @@
+import pyautogui
+import time
+pyautogui.PAUSE = 1
+
+pyautogui.press("win")
+pyautogui.write("spotify")
+pyautogui.press("enter")
+
+time.sleep(5)
+
+pyautogui.click(x=639, y=611)
+
+time.sleep(10)
+
+pyautogui.click(x=688, y=607)
+
+time.sleep(10)
+
+pyautogui.click(x=688, y=607)
+
+time.sleep(10)
+
+pyautogui.click(x=688, y=607)
